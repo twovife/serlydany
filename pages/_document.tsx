eec6f39fa2f5c3952abd514969901b7b7f0bd226
twovife <78835047+twovife@users.noreply.github.com>
2024-08-09 00:41:26 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,9 +20,9 @@ export default class Document extends NextDocument {
                         content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."></meta>
                     <meta property="og:locale" content="id_ID" />
                     <meta property="og:site_name" content="Undangan Pernikahan" />
-                    <meta property="og:url" content="https://serlydanytest.vercel.app" />
-                    <meta property="og:image" content="https://serlydanytest.vercel.app/covericon.png" />
-                    <meta property="twitter:image" content="https://serlydanytest.vercel.app/covericon.png" />
+                    <meta property="og:url" content="https://serlydany.vercel.app" />
+                    <meta property="og:image" content="https://serlydany.vercel.app/covericon.png" />
+                    <meta property="twitter:image" content="https://serlydany.vercel.app/covericon.png" />
                     <meta property="twitter:card" content="summary_large_image"></meta>
                     <meta property="twitter:title" content="Undangan Pernikahan Sherly ❤️ Dany"></meta>
                     <meta property="og:image:width" content="500" />
