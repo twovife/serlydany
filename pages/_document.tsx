5adf483b3849ba020dd36c3771107980e1957ba5
twovife <78835047+twovife@users.noreply.github.com>
2024-08-14 17:15:02 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,9 +12,9 @@ export default class Document extends NextDocument {
                     <meta property="twitter:description" content="28 Agustus 2024"></meta>
                     <meta property="og:locale" content="id_ID" />
                     <meta property="og:site_name" content="Undangan Pernikahan" />
-                    <meta property="og:url" content="https://serlydany.vercel.app" />
-                    <meta property="og:image" content="https://serlydany.vercel.app/covericon.png" />
-                    <meta property="twitter:image" content="https://serlydany.vercel.app/covericon.png" />
+                    <meta property="og:url" content="https://sherlydany.vercel.app" />
+                    <meta property="og:image" content="https://sherlydany.vercel.app/covericon.png" />
+                    <meta property="twitter:image" content="https://sherlydany.vercel.app/covericon.png" />
                     <meta property="twitter:card" content="summary_large_image"></meta>
                     <meta property="twitter:title" content="The Wedding of Sherly & Dany"></meta>
                     <meta property="og:image:width" content="500" />
